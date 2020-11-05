3657e5373a776ae98fbdbffc60d2cdd5a70eb8b1
PaintDream <paintdream@paintdream.com>
2020-11-05 19:40:30 +0800

    Support drag'n'drop.

--- Source/Utility/LeavesWing/main.cpp
+++ Source/Utility/LeavesWing/main.cpp
@@ -1,6 +1,7 @@
 #include "../LeavesFlute/LeavesFlute.h"
 #ifdef _WIN32
 #include "../../General/Driver/Debugger/MiniDump/ZDebuggerWin.h"
+#include <shlwapi.h>
 // #include <vld.h>
 #endif
 
@@ -30,7 +31,29 @@ int main(int argc, char* argv[]) {
 	RegisterDump();
 
 	CmdLine cmdLine;
+
+	// support for drag'n'drop startup profile
+#ifdef _WIN32
+	if (argc == 2 && argv[1][0] != '-') {
+		char target[MAX_PATH * 2] = "";
+		char current[MAX_PATH * 2] = "";
+		::GetCurrentDirectoryA(MAX_PATH * 2, current);
+		::PathRelativePathToA(target, current, FILE_ATTRIBUTE_DIRECTORY, argv[1], FILE_ATTRIBUTE_NORMAL);
+		String mount = String("--Mount=") + target;
+
+		char* dragArgs[] = {
+			argv[0],
+			"--Graphic=true",
+			const_cast<char*>(mount.c_str())
+		};
+		
+		cmdLine.Process(3, dragArgs);
+	} else {
+		cmdLine.Process(argc, argv);
+	}
+#else
 	cmdLine.Process(argc, argv);
+#endif
 
 	Loader loader;
 	loader.Load(cmdLine);
