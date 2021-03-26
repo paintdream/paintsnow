c6eeede0cbac9de83799fc8ee39760683f1932de
PaintDream <paintdream@paintdream.com>
2021-03-26 23:59:47 +0800

    Add basic files to DotNetBridge.

--- Extend/DotNetBridge/DotNetBridge.cpp
+++ Extend/DotNetBridge/DotNetBridge.cpp
@@ -1,4 +1,10 @@
-#include "pch.h"
-
 #include "DotNetBridge.h"
+#include "../../Source/Core/Interface/IScript.h"
 
+using namespace PaintsNow;
+class Interfaces;
+
+extern "C" __declspec(dllexport) uint64_t SetupProxy(void*, IScript::Request&, void*, const char*, uint64_t, uint64_t)
+{
+	return 0;
+}
